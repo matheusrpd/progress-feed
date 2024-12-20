9e1cedfd4362a681ffbdd4400508f1730d4b251f
matheusrpd <matheusrpd@gmail.com>
2024-12-20 12:57:31 -0300

    feat: add sign-in with google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import { useSignIn } from '@clerk/nextjs'
 export default function Home() {
   const { isLoaded, signIn } = useSignIn()
 
-  async function handleSingIn() {
+  async function handleSingIn(platform: 'github' | 'google') {
     if (!isLoaded) {
       return
     }
 
     try {
       await signIn.authenticateWithRedirect({
-        strategy: 'oauth_github',
+        strategy: platform === 'github' ? 'oauth_github' : 'oauth_google',
         redirectUrlComplete: '/feed',
         redirectUrl: '/feed',
       })
@@ -27,7 +27,8 @@ export default function Home() {
 
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <button onClick={handleSingIn}>Entrar com Github</button>
+      <button onClick={() => handleSingIn('github')}>Entrar com Github</button>
+      <button onClick={() => handleSingIn('google')}>Entrar com Google</button>
     </main>
   )
 }
